38bd6585c28d8a653a3cdbdd2c57a6f35330cbbb
firesquid6 <jdeiss06@gmail.com>
2024-04-24 20:46:23 -0500

    make tags script

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -270,31 +270,6 @@ require("lazy").setup({
     },
   },
   {
-    "firesquid6/nvim-yati",
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        yati = {
-          enable = true,
-          disable = { "gdscript", "json", "lua" },
-          -- Whether to enable lazy mode (recommend to enable this if bad indent happens frequently)
-          default_lazy = true,
-
-          -- Determine the fallback method used when we cannot calculate indent by tree-sitter
-          --   "auto": fallback to vim auto indent
-          --   "asis": use current indent as-is
-          --   "cindent": see `:h cindent()`
-          -- Or a custom function return the final indent result.
-          default_fallback = "auto",
-          suppress_conflict_warning = false,
-        },
-        indent = {
-          enable = true, -- disable builtin indent module
-          disable = { "html", "javascript", "typescript", "tsx", "python" },
-        }
-      }
-    end,
-  },
-  {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     config = true
